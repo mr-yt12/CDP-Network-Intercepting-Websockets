d1e002dc144c36ae462be6e9637a6be3721fdf51
mr-yt12 <35060206+mr-yt12@users.noreply.github.com>
2021-12-22 22:26:30 +0300

    Add files via upload

---
+++ extension/debugger.js
@@ -0,0 +1,25 @@
+const version = '1.3';
+
+function onAttach(debuggeeId) {
+  chrome.debugger.sendCommand(debuggeeId, 'Network.enable');
+}
+
+function allEventHandler(debuggeeId, message, params) {
+  if (message == 'Network.webSocketFrameSent') {
+    readSockets('sent', params, debuggeeId);
+  } else if (message == 'Network.webSocketFrameReceived') {
+    readSockets('received', params, debuggeeId);
+  }
+}
+
+function readSockets(direction, params, debuggeeId) {
+  console.log(direction, params.response.payloadData);
+}
+
+chrome.action.onClicked.addListener(function (tab) {
+  var tabId = tab.id;
+  var debuggeeId = { tabId: tabId };
+  chrome.debugger.attach(debuggeeId, version, onAttach.bind(null, debuggeeId));
+});
+
+chrome.debugger.onEvent.addListener(allEventHandler);
